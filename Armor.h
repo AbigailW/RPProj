f890e0e4ed1c88c11479c3c3a05ea3d61a13866a
Landon Abney <Landonabney@gmail.com>
2014-09-09 02:04:02 +0000

    Convert to tabs...

--- Armor.h
+++ Armor.h
@@ -5,36 +5,35 @@
 using namespace std;
 
 enum Weight {
-    light, medium, heavy, shield
+	light, medium, heavy, shield
 };
 
 enum Stealth {
-    disadvantage, advantage, none
+	disadvantage, advantage, none
 };
 
 class Armor: public Item {
-    public:
-        Armor();
-        Armor(Weight, int ACinp, int str, Stealth, string n, string des,
-              string loc, double w);
-        Armor(string nam): Item(nam) {
-        	name = nam;
-        	description = "Tested shit";
-        	location = "In a place";
-        	weight = 1000;
-        	armorWeight = shield;
-        	AC = 42;
-        	armorStealth = none;
-        	strReq = 9001;
-        }
-        string printArmorWeight(bool cap = false) const;
-        string printStealth(bool cap = false) const;
-        void printInfo() const;
-    private:
-        Weight armorWeight;
-        Stealth armorStealth;
-        int AC, strReq;
-        
+	public:
+		Armor();
+		Armor(Weight, int ACinp, int str, Stealth, string n, string des,
+			  string loc, double w);
+		Armor(string nam): Item(nam) {
+			name = nam;
+			description = "Tested shit";
+			location = "In a place";
+			weight = 1000;
+			armorWeight = shield;
+			AC = 42;
+			armorStealth = none;
+			strReq = 9001;
+		}
+		string printArmorWeight(bool cap = false) const;
+		string printStealth(bool cap = false) const;
+		void printInfo() const;
+	private:
+		Weight armorWeight;
+		Stealth armorStealth;
+		int AC, strReq;
 };
 
 #endif
--- Helper.cpp
+++ Helper.cpp
@@ -5,8 +5,8 @@
 static std::random_device rd;
 
 int rollDice(const int number, const int sides) {
-    // Choose a random mean between 1 and sides
-    std::default_random_engine e1(rd());
-    std::uniform_int_distribution<int> uniform_dist(1, sides);
-    return uniform_dist(e1);
+	// Choose a random mean between 1 and sides
+	std::default_random_engine e1(rd());
+	std::uniform_int_distribution<int> uniform_dist(1, sides);
+	return uniform_dist(e1);
 }
